d93980f3c5b6162ed537337a3d9fd85d84191135
Peter Moresi <peter.moresi@gmail.com>
2015-10-27 23:42:52 -0700

    Replace loop with d3-timer

--- src/Animate.js
+++ src/Animate.js
@@ -1,5 +1,6 @@
-import Loop from './Loop'
 import {ease} from 'd3-ease'
+import {timer, timerReplace} from 'd3-timer'
+import {interpolate} from 'd3-interpolate'
 
 let eases = [
   'linear-in',
@@ -35,13 +36,18 @@ let eases = [
 ], Easing = {};
 
 /**
- * React state animation wrapper
- *  - update state value by requestAnimationFrame loop
- */
+* React state animation wrapper
+*  - update state value by requestAnimationFrame loop
+*/
 export default class Animate {
 
-  constructor(component) {
+  /* Animation constructor accept data container and frames per second.
+  */
+  constructor(component, fps=60) {
+
+    // keep internal reference to the component
     this._component = component;
+    this._fps = fps;
 
     // generate an interface function for each ease.
     eases.forEach( (e) => {
@@ -50,99 +56,58 @@ export default class Animate {
 
       // add instance methods dynamically
       this[easeName] = function(prop, end, duration) {
-        return this.animate(prop, end, duration, easeName)
+        return this.animate(prop, end, duration, e)
       }
-
-      Easing[easeName] = ease(e)
 
     });
   }
 
   /**
-   * Get state value
-   * if the prop is not in state regular property
-   */
+  * Get state value
+  * if the prop is not in state regular property
+  */
   _getStateValue(prop) {
     var c = this._component,
-        v = c.state && c.state[prop]
+    v = c.state && c.state[prop]
     return v === undefined ? c[prop] : v
   }
 
   /**
-   * Set value to state
-   * if the prop is not in state, set value to regular property with force update
-   */
-  _updateStateValue(prop, v) {
-    return new Promise((resolve, reject) => {
-      var c = this._component
-      if(c.state && c.state[prop] !== undefined){
-        var state = {}
-        state[prop] = v
-        c.setState(state, resolve)
-      }else{
-        c[prop] = v
-        c.forceUpdate()
-          resolve()
-      }
-    })
-  }
-
-  _start(loopCallback) {
-    this._loop = new Loop(loopCallback)
-      this._loop.start()
+  * Set value to state
+  * if the prop is not in state, set value to regular property with force update
+  */
+  _updateStateValue(prop, v, resolve) {
+    var c = this._component
+    if(c.state && c.state[prop] !== undefined){
+      var state = {}
+      state[prop] = v
+      c.setState(state)
+    }else{
+      c[prop] = v
+      c.forceUpdate()
+    }
   }
 
   animate(prop, end, duration, easing) {
-    if(!Easing[easing]) {
-      console.log("Specified easing does not exist: " + easing)
-        return
-    }
+
     return new Promise((resolve, reject) => {
-      var begin = this._getStateValue(prop)
-        this._start(() => {
-          return this._anim(prop, begin, end, duration, easing, resolve)
-        })
+      var begin = this._getStateValue(prop),
+      i = interpolate(begin, end),
+      easeFun = ease(easing)
+
+      timer( (elapsed,d) => {
+        var progress = easeFun(elapsed / 1000),
+        value = i(progress)
+
+        this._updateStateValue(prop, value, resolve)
+
+        if (elapsed > 1000) {
+          this._updateStateValue(prop, end, resolve)
+          resolve()
+          return true;
+        }
+
+      })
     })
   }
-
-  // for override on each loop
-  onProcess(prop, value, progress) {
-  }
-
-  /**
-   * Start animation
-   *  - prop is a react state property
-   *  - end is a goal value of the state
-   */
-  _anim(prop, begin, end, duration, easing, resolve) {
-    if(!this._loop){
-      resolve()
-        return false
-    }
-    var progress = Easing[easing](this._loop.timeDiff() / duration),
-        distance = Math.abs(begin - end),
-        diff = progress * distance,
-        operator = begin > end ? -1 : 1,
-        value = begin + diff * operator
-    this.onProcess(prop, value, progress)
-      if(progress < 1) {
-        // return promise to keep loop
-        return this._updateStateValue(prop, value)
-      }else{
-        this.stop()
-          this._updateStateValue(prop, end).then(() => {
-            resolve()
-          })
-          return false
-      }
-  }
-
-  stop() {
-    if(this._loop) {
-      this._loop.end()
-        this._loop = null
-    }
-    return this
-  }
-
 }
